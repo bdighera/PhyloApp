1b2b12418955bcbd935d1f14685c50b584c2d9bd
Bryan Dighera <bdighera@csu.fullerton.edu>
2021-03-13 22:33:24 -0800

    Delete connection.php

--- templates/connection.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "Records";
-$conn = mysqli_connect($dbServername, $dbUsername, $dbUsername, $dbPassowrd, $dbName);	
-?>
