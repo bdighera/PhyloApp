a5de759ef7dc45d9d710d0fc1ac1665065722f92
bdighera <bdighera@csu.fullerton.edu>
2022-02-27 20:54:03 -0800

    front-end updates to MSA figure

--- static/msa.js
+++ static/msa.js
@@ -1,3 +1,18 @@
 /**
  * Created by Bryan on 2/27/22.
  */
+
+function formatMSA(msa) {
+
+    seqArray = Array.from(msa)
+    // console.log(seqArray)
+    seqArray.forEach(function (item, index) {
+        const square = document.createElement("div");
+        const newContent = document.createTextNode(item);
+        square.appendChild(newContent);
+        // console.log(item, index);
+        const currentDiv = document.getElementById("msa_subset");
+        // document.body.appendChild(square);
+    });
+}
+
