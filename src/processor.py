df503bfc5d91fa91c1d451ffcccf76443e664162
bdighera <bdighera@csu.fullerton.edu>
2022-08-18 19:02:05 -0700

    Rolling back testing of os flexibility

--- src/processor.py
+++ src/processor.py
@@ -9,7 +9,6 @@ import toytree, toyplot
 from collections import ChainMap
 from Bio import pairwise2, SeqIO
 from Bio.pairwise2 import format_alignment
-import platform
 
 class PhyloTreeConstruction(object):
 
@@ -82,22 +81,14 @@ class PhyloTreeConstruction(object):
                 msaprotein_writeFile.write(protein)
                 self.msa.append(str(protein_sequence[i]))
 
-        if platform.system() != 'Linux': #Mac pathway
-            clustalomega_cline = ClustalOmegaCommandline(cmd=os.path.join('execs','Mac', "clustalo-1.2.0"),
-                                                         infile=in_file,
-                                                         outfile=out_file, verbose=True, auto=True, force=True)
-            clustalomega_cline()
+        clustalomega_cline = ClustalOmegaCommandline(cmd=os.path.join('execs', "clustalo-1.2.0"),
+                                                     infile=in_file,
+                                                     outfile=out_file, verbose=True, auto=True, force=True)
+        clustalomega_cline()
 
-            self.MSA = SeqIO.parse(out_file, 'fasta')
-            return self.MSA
-        if platform.system() == 'Linux':
-            clustalomega_cline = ClustalOmegaCommandline(cmd=os.path.join('execs','Linux', "clustalo-1.2.0"),
-                                                         infile=in_file,
-                                                         outfile=out_file, verbose=True, auto=True, force=True)
-            clustalomega_cline()
+        self.MSA = SeqIO.parse(out_file, 'fasta')
+        return self.MSA
 
-            self.MSA = SeqIO.parse(out_file, 'fasta')
-            return self.MSA
     def constructTreeObj(self):
 
         tree = handler.treeOBjFileHandler()
